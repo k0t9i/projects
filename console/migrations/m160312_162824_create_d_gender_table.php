27eae25805236450802dedd8a87626aec8310ff7
k0t9i <k0t9i@yandex.ru>
2016-03-12 22:44:59 +0500

    Add migrations: create d_gender and user tables

---
+++ console/migrations/m160312_162824_create_d_gender_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m160312_162824_create_d_gender_table extends Migration {
+
+    public function up()
+    {
+        $this->createTable('d_gender', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(256)->notNull()->unique()
+        ]);
+        $this->insert('d_gender', [
+            'name' => 'male'
+        ]);
+        $this->insert('d_gender', [
+            'name' => 'female'
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('d_gender');
+    }
+
+}
+
+
---
+++ console/migrations/m160312_163824_create_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160312_163824_create_user_table extends Migration {
+
+    public function up()
+    {        
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'login' => $this->string(256)->notNull()->unique(),
+            'password' => $this->string(64)->notNull(),
+            'lastname' => $this->string(1024),
+            'firstname' => $this->string(1024),
+            'middlename' => $this->string(1024),
+            'id_gender' => $this->integer(),
+            'email' => $this->string(256)->notNull()->unique(),
+            'lastLogin' => $this->dateTime(),
+        ]);
+        $this->createIndex('idx-user-id_gender', 'user', 'id_gender');
+        $this->addForeignKey('fk-user-d_gender', 'user', 'id_gender', 'd_gender', 'id');
+    }
+
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+
+}
