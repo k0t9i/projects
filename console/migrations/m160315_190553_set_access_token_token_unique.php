ab4011719e95616e13296f732609932bf14612b5
k0t9i <k0t9i@yandex.ru>
2016-03-16 00:09:29 +0500

    Token value sets unique

---
+++ console/migrations/m160315_190553_set_access_token_token_unique.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m160315_190553_set_access_token_token_unique extends Migration
+{
+    public function up()
+    {
+        $this->createIndex('idx-access_token-token', 'access_token', 'token', true);
+    }
+
+    public function down()
+    {
+        $this->dropIndex('idx-access_token-token', 'access_token');
+    }
+}
