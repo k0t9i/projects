85f6076b3e0f833091af18edd13728086b95ab91
k0t9i <k0t9i@yandex.ru>
2016-03-18 22:51:24 +0500

    Add roles for user actions

--- api/common/controllers/UserController.php
+++ api/common/controllers/UserController.php
@@ -3,9 +3,49 @@
 namespace api\common\controllers;
 
 use api\common\models\User;
+use yii\filters\AccessControl;
 
 class UserController extends ApiController
 {
+    
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['access'] = [
+            'class' => AccessControl::className(),
+            'except' => ['options', 'self'],
+            'rules' => [
+                [
+                    'allow' => true,
+                    'actions' => ['index'],
+                    'roles' => ['user.viewAll']
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['view'],
+                    'roles' => ['user.view']
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['delete'],
+                    'roles' => ['user.delete']
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['update'],
+                    'roles' => ['user.update']
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['create'],
+                    'roles' => ['user.create']
+                ]
+            ]
+        ];
+
+        return $behaviors;
+    }
 
     public function verbs()
     {
--- console/models/Rbac.php
+++ console/models/Rbac.php
@@ -4,19 +4,24 @@ namespace console\models;
 
 use yii\base\Model;
 use api\rbac\OwnerRule;
+use yii\rbac\ManagerInterface;
 
 class Rbac extends Model
 {
 
     public function initRoles()
     {
-        \yii\rbac\ManagerInterface::class;
+        /* @var $auth \yii\rbac\ManagerInterface */
         $auth = \Yii::$app->authManager;
         
         $auth->removeAll();
         
-        $rule = new OwnerRule();
-        $auth->add($rule);
+        $rules = [
+            'ownerRule' => new OwnerRule()
+        ];
+        foreach ($rules as $rule) {
+            $auth->add($rule);
+        }
         
         $performer = $auth->createRole('performer');
         $performer->description = 'Project performer';
@@ -33,51 +38,98 @@ class Rbac extends Model
         $admin = $auth->createRole('admin');
         $admin->description = 'Administrator';
         $auth->add($admin);
-
-        $deleteAll = $auth->createPermission('access-token.deleteAll');
-        $deleteAll->description = 'Delete all access tokens';
-        $auth->add($deleteAll);
         
-        $delete = $auth->createPermission('access-token.delete');
-        $delete->description = 'Delete access token';
-        $auth->add($delete);
+        $tokenRoles = $this->initAccessTokenRoles($auth, 'access-token', $rules);
+        $userRoles = $this->initUserRoles($auth, 'user', $rules);
         
-        $deleteOwn = $auth->createPermission('access-token.deleteOwn');
-        $deleteOwn->description = 'Delete own access token';
-        $deleteOwn->ruleName = $rule->name;
-        $auth->add($deleteOwn);
-        $auth->addChild($deleteOwn, $delete);
+        $auth->addChild($performer, $tokenRoles['deleteOwn']);
+        $auth->addChild($performer, $tokenRoles['viewOwn']);
         
-        $viewAll = $auth->createPermission('access-token.viewAll');
-        $viewAll->description = 'View all access tokens';
-        $auth->add($viewAll);
+        $auth->addChild($manager, $tokenRoles['deleteOwn']);
+        $auth->addChild($manager, $tokenRoles['viewOwn']);
         
-        $view = $auth->createPermission('access-token.view');
-        $view->description = 'View access token';
-        $auth->add($view);
+        $auth->addChild($chief, $tokenRoles['deleteOwn']);
+        $auth->addChild($chief, $tokenRoles['viewOwn']);
+        $auth->addChild($chief, $userRoles['view']);
+        $auth->addChild($chief, $userRoles['viewAll']);
+        $auth->addChild($chief, $userRoles['create']);
+        $auth->addChild($chief, $userRoles['update']);
+        $auth->addChild($chief, $userRoles['delete']);
         
-        $viewOwn = $auth->createPermission('access-token.viewOwn');
-        $viewOwn->description = 'View own access token';
-        $viewOwn->ruleName = $rule->name;
-        $auth->add($viewOwn);
-        $auth->addChild($viewOwn, $view);
+        $auth->addChild($admin, $tokenRoles['delete']);
+        $auth->addChild($admin, $tokenRoles['deleteAll']);
+        $auth->addChild($admin, $tokenRoles['view']);
+        $auth->addChild($admin, $tokenRoles['viewAll']);
+        $auth->addChild($admin, $userRoles['view']);
+        $auth->addChild($admin, $userRoles['viewAll']);
+        $auth->addChild($admin, $userRoles['create']);
+        $auth->addChild($admin, $userRoles['update']);
+        $auth->addChild($admin, $userRoles['delete']);
         
-        $auth->addChild($performer, $deleteOwn);
-        $auth->addChild($performer, $viewOwn);
-        
-        $auth->addChild($manager, $deleteOwn);
-        $auth->addChild($manager, $viewOwn);
-        
-        $auth->addChild($chief, $deleteOwn);
-        $auth->addChild($chief, $viewOwn);
-        
-        $auth->addChild($admin, $delete);
-        $auth->addChild($admin, $deleteAll);
-        $auth->addChild($admin, $view);
-        $auth->addChild($admin, $viewAll);
-        
-        //$auth->assign($admin, 1);
+        $auth->assign($admin, 1);
         //$auth->assign($performer, 3);
+    }
+    
+    private function initAccessTokenRoles(ManagerInterface $manager, $key, array $rules)
+    {
+        $ret = [];
+        
+        $ret['deleteAll'] = $manager->createPermission($key . '.deleteAll');
+        $ret['deleteAll']->description = 'Delete all access tokens';
+        $manager->add($ret['deleteAll']);
+        
+        $ret['delete'] = $manager->createPermission($key . '.delete');
+        $ret['delete']->description = 'Delete access token';
+        $manager->add($ret['delete']);
+        
+        $ret['deleteOwn'] = $manager->createPermission($key . '.deleteOwn');
+        $ret['deleteOwn']->description = 'Delete own access token';
+        $ret['deleteOwn']->ruleName = $rules['ownerRule']->name;
+        $manager->add($ret['deleteOwn']);
+        $manager->addChild($ret['deleteOwn'], $ret['delete']);
+        
+        $ret['viewAll'] = $manager->createPermission($key . '.viewAll');
+        $ret['viewAll']->description = 'View all access tokens';
+        $manager->add($ret['viewAll']);
+        
+        $ret['view'] = $manager->createPermission($key . '.view');
+        $ret['view']->description = 'View access token';
+        $manager->add($ret['view']);
+        
+        $ret['viewOwn'] = $manager->createPermission($key . '.viewOwn');
+        $ret['viewOwn']->description = 'View own access token';
+        $ret['viewOwn']->ruleName = $rules['ownerRule']->name;
+        $manager->add($ret['viewOwn']);
+        $manager->addChild($ret['viewOwn'], $ret['view']);
+        
+        return $ret;        
+    }
+    
+    private function initUserRoles(ManagerInterface $manager, $key, array $rules)
+    {
+        $ret = [];
+        
+        $ret['create'] = $manager->createPermission($key . '.create');
+        $ret['create']->description = 'Create user';
+        $manager->add($ret['create']);
+        
+        $ret['viewAll'] = $manager->createPermission($key . '.viewAll');
+        $ret['viewAll']->description = 'View all users';
+        $manager->add($ret['viewAll']);
+        
+        $ret['view'] = $manager->createPermission($key . '.view');
+        $ret['view']->description = 'View user';
+        $manager->add($ret['view']);
+        
+        $ret['update'] = $manager->createPermission($key . '.update');
+        $ret['update']->description = 'Update user';
+        $manager->add($ret['update']);
+        
+        $ret['delete'] = $manager->createPermission($key . '.delete');
+        $ret['delete']->description = 'Delete user';
+        $manager->add($ret['delete']);
+        
+        return $ret;        
     }
 
 }
