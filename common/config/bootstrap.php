c5cc5859eeba2eb195164fd90c91bc3e0bacf1de
k0t9i <k0t9i@yandex.ru>
2016-03-20 22:40:04 +0500

    Add tests alias

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -2,3 +2,4 @@
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@tests', dirname(dirname(__DIR__)) . '/tests');
