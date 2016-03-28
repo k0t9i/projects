8e0e17db19ab332b9b20c4abef5c6117af1970a1
k0t9i <k0t9i@yandex.ru>
2016-03-28 13:54:52 +0400

    Setup

--- tests/functional/AccessTokenCest.php
+++ tests/functional/AccessTokenCest.php
@@ -9,16 +9,6 @@ class AccessTokenCest
     public function _after(FunctionalTester $I)
     {
     }
-
-    public function createAccessTokenWithInvalidCredentials(FunctionalTester $I)
-    {
-        $I->wantTo('create access token with invalid credentials');
-        $I->sendPOST('/access-tokens', [
-            'login' => 'this.is.invalid.login',
-            'password' => 'this.is.invalid.password'
-        ]);
-        $I->seeResponseCodeIs(403);
-    }
     
     public function createAccessToken(FunctionalTester $I)
     {
@@ -27,16 +17,29 @@ class AccessTokenCest
             'login' => 'admin',
             'password' => 'admin'
         ]);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([]);
         $I->seeResponseCodeIs(201);
-    }
-    
-    public function createAccessTokenByDisabledUser(FunctionalTester $I)
-    {
-        $I->wantTo('create access token by disabled user');
+        
+        $I->sendPOST('/access-tokens', [
+            'login' => 'this.is.invalid.login',
+            'password' => 'this.is.invalid.password'
+        ]);
+        $I->seeResponseCodeIs(403);
+        
         $I->sendPOST('/access-tokens', [
             'login' => 'disabled',
             'password' => 'disabled'
         ]);
         $I->seeResponseCodeIs(403);
+    }
+    
+    public function viewAccessTokenList(FunctionalTester $I)
+    {
+        $I->wantTo('view access token list');
+        $I->sendGET('/access-tokens', [
+            'access-token' => 'admin-token'
+        ]);
+        $I->seeResponseCodeIs(200);
     }
 }
